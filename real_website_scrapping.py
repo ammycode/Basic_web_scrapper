a3144e20bda2cfce15141d942b7ac683d3d99db7
ammycode <amitbinjolaw@gmail.com>
2023-04-13 00:35:33 +0530

    scraping company names from stackoverflow, finding description of company

--- real_website_scrapping.py
+++ real_website_scrapping.py
@@ -12,6 +12,13 @@ response_code= requests.get(url)
 html_text=requests.get(url).text
 soup= bs(html_text,'lxml')
 
-company_html= soup.find('h2',class_='fs-body2 mb4')
-company_name=company_html.find('a').text
-print(company_name)
+'''
+<p class="mt8 mb0 fs-body1 fc-black-700 v-truncate2">Industry-leading. Difference-making. Tomorrow-shaping. These are just few of the adjectives used to describe the technologies and companies we work with every day. Join Deloitte, and those adjectives could also be describing your career.
+Discover your impact
+Make the most of disruptive technologies. Find the value in emerging business models. Turn technology from something...</p>'''
+
+company_html1= soup.find('h2',class_='fs-body2 mb4')
+
+company_descrption=soup.find('p',class_='mt8 mb0 fs-body1 fc-black-700 v-truncate2').text.split('\n')
+company_name=company_html1.find('a').text
+print(company_descrption[0])
